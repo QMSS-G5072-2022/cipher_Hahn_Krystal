14678162176c707884be00a01d838a99f9bc540a
Krystal Hahn <kmh2259@columbia.edu>
2022-10-31 21:41:06 -0400

    Added test and pytest

--- tests/test_cipher_kmh2259.py
+++ tests/test_cipher_kmh2259.py
@@ -3,6 +3,6 @@ from cipher_kmh2259 import cipher_kmh2259
 def test_cipher_single():
     example = 'apple'
     expected = 'dssoh'
-    actual = cipher_kmh2259(example, 3)
+    actual = cipher_kmh2259.cipher(example, 3)
     assert actual == expected, "Encrypted word should be 'dssoh'"
 test_cipher_single()
